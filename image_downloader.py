9a00621284cff84a435877d50c491936d8170f13
Rishith01 <rishithgupta01@gmail.com>
2024-06-24 11:49:20 +0530

    Updated Image Dataset

--- image_downloader.py
+++ image_downloader.py
@@ -2,13 +2,13 @@
 from bing_image_downloader import downloader
 
 # add your class name
-class_name = 'cat'
+class_name = 'chair'
 
 # no of photos to download
 no_of_photos = 50
 
 # Images download directory
-output_directory = 'Dataset_Images'
+output_directory = 'Pretraining_Dataset_Images'
 
 # Run the program
 downloader.download(query=class_name, limit=no_of_photos,  output_dir=output_directory)
